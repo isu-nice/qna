a6b274936f44afff4014d5fe4a9e4f8fa9c5923e
isu-nice <isu010614@naver.com>
2023-05-17 15:45:44 +0900

    feat: 탐색 기능 추가

--- src/main/java/qna/answer/repository/AnswerRepository.java
+++ src/main/java/qna/answer/repository/AnswerRepository.java
@@ -2,7 +2,11 @@ package qna.answer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import qna.answer.entity.Answer;
+import qna.question.entity.Question;
+
+import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    Optional<Answer> findAnswerByQuestion(Question question);
     // TODO 쿼리 추가
 }
